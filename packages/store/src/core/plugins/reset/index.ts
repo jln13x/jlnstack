bdb64cbc0a132c82c1fb4891e330bf8883920f32
jln <85513960+jln13x@users.noreply.github.com>
2026-01-10 13:05:42 +0100

    resettable

--- packages/store/src/core/plugins/reset/index.ts
+++ packages/store/src/core/plugins/reset/index.ts
@@ -3,13 +3,21 @@ import { definePlugin } from "../plugin";
 
 export function reset() {
   return definePlugin(<TState>(store: StoreApi<TState>) => {
+    let changed = false;
     const initialState = store.getState();
     return {
       id: "reset",
+      onStateChange: () => {
+        changed = true;
+      },
       extend: {
         reset: () => {
           store.setState(initialState);
+          changed = false;
           return initialState;
+        },
+        canReset: () => {
+          return changed;
         },
       },
     };
